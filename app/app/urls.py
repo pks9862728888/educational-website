fa40eb03c52c061517603ffc50c6507a3e6b6b82
root <root@localhost.localdomain>
2020-04-26 14:45:56 +0530

    Added get auth token functionality

--- app/app/urls.py
+++ app/app/urls.py
@@ -17,10 +17,9 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path, include
 from django.contrib import admin
-from rest_framework.authtoken.views import obtain_auth_token
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('get-auth-token/', obtain_auth_token, name='auth-token'),
     path('user/', include('user.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- app/user/urls.py
+++ app/user/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from rest_framework.authtoken.views import obtain_auth_token
 
 from . import views
 
@@ -6,4 +7,5 @@ app_name = 'user'
 
 urlpatterns = [
     path('signup', views.CreateUserView.as_view(), name='user-signup'),
+    path('get-auth-token', obtain_auth_token, name='get-auth-token'),
 ]
