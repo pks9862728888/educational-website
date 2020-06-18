7bc13d5c5ac9effbac3c6a80fd74617cd1266e3c
root <root@localhost.localdomain>
2020-06-18 21:04:28 +0530

    Added broken files

--- app/teacher/serializer.py
+++ app/teacher/serializer.py
@@ -59,6 +59,7 @@ class ManageTeacherProfileSerializer(serializers.ModelSerializer):
         """Add or modify details of user"""
         profile_data = validated_data.pop('teacher_profile', None)
         validated_data.pop('profile_pictures', None)
+        validated_data.pop('email', None)
         profile = instance.teacher_profile
 
         user = super().update(instance, validated_data)
