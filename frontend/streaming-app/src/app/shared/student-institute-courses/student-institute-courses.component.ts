1741e56f65c1ca71096c7b92cce5720f3d154725
root <pks9862728888@gmail.com>
2020-09-09 04:19:04 +0530

    .

---
+++ frontend/streaming-app/src/app/shared/student-institute-courses/student-institute-courses.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-student-institute-courses',
+  templateUrl: './student-institute-courses.component.html',
+  styleUrls: ['./student-institute-courses.component.css']
+})
+export class StudentInstituteCoursesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
